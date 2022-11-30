ae925d3eabb8670a40f9da1e3be86365c2c796cc
TheGamer3514 <thegamer3514@gmail.com>
2022-11-30 18:41:03 +0000

    Create userinfo.py

---
+++ userinfo.py
@@ -0,0 +1,4 @@
+name = input("What is your name: ")
+colour = input("What is your favourite colour: ")
+team = input("What is your favourite football team: ")
+print(name + "'s favourite colour is " + colour + " and favourite football team is " + team)
